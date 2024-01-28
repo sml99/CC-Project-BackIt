ecfc2aa6cf304f2074ddb1f974df7e3ceecf7cfc
Dahmani Smail <smail.dahmani.98@gmail.com>
2024-01-28 23:39:28 +0100

    feat: add comment model

---
+++ app/backend/models/comment.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    comment: {
+        type: String,
+        required: [true, 'Please enter a comment'],
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    project: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Project',
+        required: [true, 'Please enter a project']
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Please enter a user']
+    }
+});
